136a7efcd27e3d46050a14ebaa3dbac25d23a1e2
Igor Kislyuk <i.kisluk@corp.mail.ru>
2018-02-03 00:06:25 +0300

    Initial Commit

---
+++ Codewars/Codewars.h
@@ -0,0 +1,19 @@
+//
+//  Codewars.h
+//  Codewars
+//
+//  Created by Igor Kislyuk on 03/02/2018.
+//  Copyright © 2018 Igor Kislyuk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Codewars.
+FOUNDATION_EXPORT double CodewarsVersionNumber;
+
+//! Project version string for Codewars.
+FOUNDATION_EXPORT const unsigned char CodewarsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Codewars/PublicHeader.h>
+
+
